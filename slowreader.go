e089c291eb7ba49071503a52d2195b78d0ddd287
Leibovich, Elazar <elazarl@gmail.com>
2012-10-21 23:03:42 +0200

    stats + ensure echo server

--- slowreader.go
+++ slowreader.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
+	"log"
 	"net"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -27,9 +29,14 @@ func main() {
 		return
 	}
 
-	var step1, step2 sync.WaitGroup
+	var step1, step2, step3 sync.WaitGroup
 	step1.Add(*nthreads)
 	step2.Add(1)
+	step3.Add(*nthreads)
+
+	var conc int32 = 0
+	var maxConc int32 = 0
+	var total int32 = 0
 	for i:=0; i<*nthreads; i++ {
 		go func() {
 			conn, err := net.Dial("tcp", *host)
@@ -37,19 +44,32 @@ func main() {
 				fmt.Println("dial:", err)
 				step1.Done()
 				step2.Wait()
+				step3.Done()
 				return
 			}
-			//panicOnErr("dial", err)
+			c := atomic.AddInt32(&conc, 1)
+			atomic.AddInt32(&total, 1)
+			for c>maxConc && !atomic.CompareAndSwapInt32(&maxConc, maxConc, c) {
+			}
+			defer step3.Done()
+			defer atomic.AddInt32(&conc, -1)
 			defer conn.Close()
 			fmt.Fprint(conn, *data)
 			step1.Done()
 			step2.Wait()
-			_, err = io.Copy(ioutil.Discard, conn)
+			out, err := ioutil.ReadAll(conn)
 			panicOnErr("copy", err)
+			if bytes.Compare(out, []byte(*data))!=0 {
+				log.Fatalln("This is not an echo server: "+string(out))
+			}
 		}()
 		time.Sleep(time.Microsecond)
 	}
 	step1.Wait()
 	step2.Done()
+	step3.Wait()
+	fmt.Println("Max conc:", maxConc)
+	fmt.Println("total:", total)
+	fmt.Println("conc:", conc)
 }
 
